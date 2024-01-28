721fe87cb9664397e08c45e6ec081a1bb3d5208e
cc299792458 <cc2018011855@gmail.com>
2024-01-27 19:24:44 -0800

    make replaybuffer explicit

--- dqn.py
+++ dqn.py
@@ -11,21 +11,60 @@ class DeepQNetwork(nn.Module):
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.model = nn.Sequential(
+            nn.Linear(*self.input_dims, self.fc1_dims),
+            nn.ReLU(),
+            nn.Linear(self.fc1_dims, self.fc2_dims),
+            nn.ReLU(),
+            nn.Linear(self.fc2_dims, self.n_actions),
+        )
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
     def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        actions = self.fc3(x)
+        q = self.model(state)
 
-        return actions
+        return q
+
+class ReplayBuffer:
+    def __init__(self, input_dims, batch_size, mem_size=100_000):
+        self.batch_size = batch_size
+        self.mem_size = mem_size
+        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool8)
+        self.mem_cntr = 0
     
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.reward_memory[index] = reward
+        self.action_memory[index] = action
+        self.terminal_memory[index] = done
+
+        self.mem_cntr += 1
+    
+    def generate_batch(self, device):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+
+        state_batch = T.tensor(self.state_memory[batch]).to(device)
+        new_state_batch = T.tensor(self.new_state_memory[batch]).to(device)
+        reward_batch = T.tensor(self.reward_memory[batch]).to(device)
+        terminal_batch = T.tensor(self.terminal_memory[batch]).to(device)
+
+        action_batch = self.action_memory[batch]
+
+        return batch_index, state_batch, new_state_batch, reward_batch, terminal_batch, action_batch
+
+
 class DQN():
     def __init__(self, gamma, epsilon, lr, input_dims, n_actions, batch_size,
                  max_mem_size=100_000, eps_min=0.01, eps_dec=5e-4):
@@ -35,56 +74,59 @@ class DQN():
         self.eps_dec = eps_dec
         self.lr = lr
         self.action_space = [i for i in range(n_actions)]
-        self.max_mem_size = max_mem_size
-        self.batch_size = batch_size
-        self.mem_cntr = 0
+        # self.max_mem_size = max_mem_size
+        # self.batch_size = batch_size
+        # self.mem_cntr = 0
 
         self.Q_eval = DeepQNetwork(self.lr, n_actions=n_actions, input_dims=input_dims,
                                    fc1_dims=256, fc2_dims=256)
-        self.state_memory = np.zeros((self.max_mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.max_mem_size, *input_dims), dtype=np.float32)
-        self.action_memory = np.zeros(self.max_mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.max_mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.max_mem_size, dtype=np.bool8)
+        self.replaybuffer = ReplayBuffer(input_dims=input_dims, batch_size=batch_size, mem_size=max_mem_size)
+        # self.state_memory = np.zeros((self.max_mem_size, *input_dims), dtype=np.float32)
+        # self.new_state_memory = np.zeros((self.max_mem_size, *input_dims), dtype=np.float32)
+        # self.action_memory = np.zeros(self.max_mem_size, dtype=np.int32)
+        # self.reward_memory = np.zeros(self.max_mem_size, dtype=np.float32)
+        # self.terminal_memory = np.zeros(self.max_mem_size, dtype=np.bool8)
         
     def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.max_mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = done
+        self.replaybuffer.store_transition(state=state, action=action, reward=reward, state_=state_, done=done)
+        # index = self.mem_cntr % self.max_mem_size
+        # self.state_memory[index] = state
+        # self.new_state_memory[index] = state_
+        # self.reward_memory[index] = reward
+        # self.action_memory[index] = action
+        # self.terminal_memory[index] = done
 
-        self.mem_cntr += 1
+        # self.mem_cntr += 1
 
     def choose_action(self, observation):
         if np.random.random() > self.epsilon:
             state = T.tensor([observation]).to(self.Q_eval.device)
-            actions = self.Q_eval.forward(state)
-            action = T.argmax(actions).item()
+            q = self.Q_eval.forward(state)
+            action = T.argmax(q).item()
         else:
             action = np.random.choice(self.action_space)
-            # action = T.tensor(action).to(self.Q_eval.device)
 
         return action
     
     def learn(self):
-        if self.mem_cntr < self.batch_size:
+        if self.replaybuffer.mem_cntr < self.replaybuffer.batch_size:
             return
         
         self.Q_eval.optimizer.zero_grad()
         
-        max_mem = min(self.mem_cntr, self.max_mem_size)
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+        # max_mem = min(self.mem_cntr, self.max_mem_size)
+        # batch = np.random.choice(max_mem, self.batch_size, replace=False)
 
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        # batch_index = np.arange(self.batch_size, dtype=np.int32)
 
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+        # state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
+        # new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+        # reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
+        # terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
 
-        action_batch = self.action_memory[batch]
+        # action_batch = self.action_memory[batch]
+
+        batch_index, state_batch, new_state_batch, reward_batch, terminal_batch, action_batch = self.replaybuffer.generate_batch(self.Q_eval.device)
 
         q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
         q_next = self.Q_eval.forward(new_state_batch)
@@ -102,8 +144,14 @@ class DQN():
 if __name__ == '__main__':
     import gym
     env = gym.make('LunarLander-v2')
-    agent = DQN(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
-                eps_min=0.01, input_dims=[8], lr=0.003)
+
+    agent = DQN(gamma=0.99,
+                epsilon=1.0, 
+                lr=0.003, 
+                input_dims=[env.observation_space.shape[0]],
+                n_actions=env.action_space.n,
+                batch_size=64,
+                eps_min=0.01)
     scores, eps_history = [], []
     n_games = 1000
 
